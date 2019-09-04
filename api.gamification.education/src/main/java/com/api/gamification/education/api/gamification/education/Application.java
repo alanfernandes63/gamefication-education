a81a0d4d22ea4305d0908895ef324b37ae266eec
alan <alan.fernandes63@gmail.com>
2019-09-04 16:13:34 -0300

    Adcionando usuario admin ao executar a aplicacao

--- api.gamification.education/src/main/java/com/api/gamification/education/api/gamification/education/Application.java
+++ api.gamification.education/src/main/java/com/api/gamification/education/api/gamification/education/Application.java
@@ -1,15 +1,40 @@
 package com.api.gamification.education.api.gamification.education;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.api.gamification.education.api.gamification.education.model.Admin;
+import com.api.gamification.education.api.gamification.education.model.UserType;
+import com.api.gamification.education.api.gamification.education.repository.AdminRepository;
 
 
 @SpringBootApplication
 public class Application {
+	@Autowired
+	AdminRepository adminRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		
+		
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			Admin admin = new Admin();
+			admin.setName("admin1");
+			admin.setUserName("admin1");
+			admin.setPassword("admin1");
+			admin.setUserType(UserType.ADMIN);
+			adminRepository.save(admin);
+			
+			
+		};
 	}
 
 }
---
+++ api.gamification.education/src/main/java/com/api/gamification/education/api/gamification/education/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.api.gamification.education.api.gamification.education.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.gamification.education.api.gamification.education.model.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Long>{
+
+}
